33c9a3faf676d33757e5d59e4c04619e25053388
Marcelo A. Amadeu <45645330+marceloamadeu@users.noreply.github.com>
2020-03-22 18:24:07 -0300

    Update app_module.dart

--- app/lib/app/app_module.dart
+++ app/lib/app/app_module.dart
@@ -1,4 +1,4 @@
-import 'package:app/app/app_controller.dart';
+import 'package:app/app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:app/app/app_widget.dart';
