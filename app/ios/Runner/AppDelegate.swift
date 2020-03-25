5ea479ccee0a41c2c44c6596c284b2ca7d1d9e44
NelsonCampos-MBP <nelsoncampos-mbp@MacBook-Pro-de-NelsonCampos-MBP.local>
2020-03-25 20:04:18 -0300

    refactor: settings the google maps api in android and ios

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBn7_7NCnQrQPIRCXCMYsKA9h0BN82dg5g")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
