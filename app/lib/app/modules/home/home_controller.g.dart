d2f9d0f1baa846c03d5925d410f0035bd1f660e9
NelsonCampos-MBP <nelsoncampos-mbp@MacBook-Pro-de-NelsonCampos-MBP.local>
2020-03-26 00:45:21 -0300

    feat: add the google maps api

--- app/lib/app/modules/home/home_controller.g.dart
+++ app/lib/app/modules/home/home_controller.g.dart
@@ -9,31 +9,58 @@ part of 'home_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$HomeController on _HomeControllerBase, Store {
-  final _$valueAtom = Atom(name: '_HomeControllerBase.value');
+  final _$markersAtom = Atom(name: '_HomeControllerBase.markers');
 
   @override
-  int get value {
-    _$valueAtom.context.enforceReadPolicy(_$valueAtom);
-    _$valueAtom.reportObserved();
-    return super.value;
+  ObservableList<dynamic> get markers {
+    _$markersAtom.context.enforceReadPolicy(_$markersAtom);
+    _$markersAtom.reportObserved();
+    return super.markers;
   }
 
   @override
-  set value(int value) {
-    _$valueAtom.context.conditionallyRunInAction(() {
-      super.value = value;
-      _$valueAtom.reportChanged();
-    }, _$valueAtom, name: '${_$valueAtom.name}_set');
+  set markers(ObservableList<dynamic> value) {
+    _$markersAtom.context.conditionallyRunInAction(() {
+      super.markers = value;
+      _$markersAtom.reportChanged();
+    }, _$markersAtom, name: '${_$markersAtom.name}_set');
+  }
+
+  final _$latLngAtom = Atom(name: '_HomeControllerBase.latLng');
+
+  @override
+  LatLng get latLng {
+    _$latLngAtom.context.enforceReadPolicy(_$latLngAtom);
+    _$latLngAtom.reportObserved();
+    return super.latLng;
+  }
+
+  @override
+  set latLng(LatLng value) {
+    _$latLngAtom.context.conditionallyRunInAction(() {
+      super.latLng = value;
+      _$latLngAtom.reportChanged();
+    }, _$latLngAtom, name: '${_$latLngAtom.name}_set');
   }
 
   final _$_HomeControllerBaseActionController =
       ActionController(name: '_HomeControllerBase');
 
   @override
-  void increment() {
+  dynamic setLatLng(LatLng _latLng) {
     final _$actionInfo = _$_HomeControllerBaseActionController.startAction();
     try {
-      return super.increment();
+      return super.setLatLng(_latLng);
+    } finally {
+      _$_HomeControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic addMarkes(String _markerId, LatLng _latLng) {
+    final _$actionInfo = _$_HomeControllerBaseActionController.startAction();
+    try {
+      return super.addMarkes(_markerId, _latLng);
     } finally {
       _$_HomeControllerBaseActionController.endAction(_$actionInfo);
     }
@@ -41,7 +68,8 @@ mixin _$HomeController on _HomeControllerBase, Store {
 
   @override
   String toString() {
-    final string = 'value: ${value.toString()}';
+    final string =
+        'markers: ${markers.toString()},latLng: ${latLng.toString()}';
     return '{$string}';
   }
 }
